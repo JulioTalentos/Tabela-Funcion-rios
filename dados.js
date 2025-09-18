2f794fed1bdfa901e552e935542a5952f096fc38
Julio <julio.moura@talentosconsultoria.com.br>
2025-09-18 16:49:22 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,42 @@
+const funcionarios = [
+    {
+        id: "01",
+        nome: "Julio Cardoso",
+        cargo: "Jovem Aprendiz de Ti",
+        documento: "172.965.857-14",
+        bancoHoras: "05:00",
+        email: "julio.moura@TalentosConsultoria.com.br"
+    },
+    {
+        id: "02",
+        nome: "Jonathan David",
+        cargo: "Auxiliar de Almoxarifado",
+        documento: "341.546.456-14",
+        bancoHoras: "00:00",
+        email: "jonathan.david@TalentosConsultoria.com.br"
+    },
+    {
+        id: "03",
+        nome: "Alexis Macallister",
+        cargo: "Auxiliar de Serviços gerais",
+        documento: "125.134.789-09",
+        bancoHoras: "03:41",
+        email: "alexis.mac@TalentosConsultoria.com.br.com"
+    },
+    {
+        id: "07",
+        nome: "Igor Jesus",
+        cargo: "Auxiliar Administrativo",
+        documento: "045.746.456-12",
+        bancoHoras: "07:00",
+        email: "igor.jesus@TalentosConsultoria.com.br"
+    },
+    {
+        id: "10",
+        nome: "Johnny Gonzales",
+        cargo: "Analista de RH",
+        documento: "134.789.133-14",
+        bancoHoras: "12:00",
+        email: "johnny.gonzales@TalentosConsultoria.com.br"
+    }
+];
